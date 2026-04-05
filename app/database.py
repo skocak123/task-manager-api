6fb93c5a6fee74d52a5f3ea40c10c72804b95138
Selim <kocakselim401@gmail.com>
2026-04-05 18:53:22 +0200

    fix model relationships and extend token expiry to 7 days

--- app/database.py
+++ app/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from app.config import settings
 
+
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
